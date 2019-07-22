28b902d4b5ce14e768256b91839ed4e95ec9e5db
Jake Shadle <jake.shadle@embark-studios.com>
2019-07-22 12:43:51 +0200

    Add get test, exercise deep paths

--- tests/objects.rs
+++ tests/objects.rs
@@ -12,7 +12,7 @@ fn insert_vanilla() {
     let insert_req = Object::insert_simple(
         &(
             &BucketName::non_validated("bucket"),
-            &ObjectName::non_validated("object"),
+            &ObjectName::non_validated("object/with/deep/path"),
         ),
         "great content",
         13,
@@ -22,7 +22,7 @@ fn insert_vanilla() {
 
     let expected = http::Request::builder()
         .method(http::Method::POST)
-        .uri("https://www.googleapis.com/upload/storage/v1/b/bucket/o?name=object&uploadType=media&prettyPrint=false")
+        .uri("https://www.googleapis.com/upload/storage/v1/b/bucket/o?name=object/with/deep/path&uploadType=media&prettyPrint=false")
         .header(http::header::CONTENT_TYPE, "application/octet-stream")
         .header(http::header::CONTENT_LENGTH, 13)
         .body("great content")
@@ -56,11 +56,28 @@ fn insert_json_content() {
 }
 
 #[test]
+fn vanilla_get() {
+    let get_req = Object::get(
+        &ObjectId::new("bucket", "test/with/path_separators").unwrap(),
+        None,
+    )
+    .unwrap();
+
+    let expected = http::Request::builder()
+        .method(http::Method::GET)
+        .uri("https://www.googleapis.com/storage/v1/b/bucket/o/test%2Fwith%2Fpath_separators?alt=json&prettyPrint=false")
+        .body(std::io::empty())
+        .unwrap();
+
+    util::requests_eq(&get_req, &expected);
+}
+
+#[test]
 fn delete_vanilla() {
     let delete_req = Object::delete(
         &(
             &BucketName::non_validated("bucket"),
-            &ObjectName::non_validated("object"),
+            &ObjectName::non_validated("object/with/deep/path"),
         ),
         None,
     )
@@ -68,7 +85,7 @@ fn delete_vanilla() {
 
     let expected = http::Request::builder()
         .method(http::Method::DELETE)
-        .uri("https://www.googleapis.com/storage/v1/b/bucket/o/object?prettyPrint=false")
+        .uri("https://www.googleapis.com/storage/v1/b/bucket/o/object%2Fwith%2Fdeep%2Fpath?prettyPrint=false")
         .body(std::io::empty())
         .unwrap();
 
